cc3c7b4f6a076c7d43cda495910446fe54d4f116
GoodnessJames <akomagoodness97@gmail.com>
2023-07-20 00:18:56 +0100

    Move all test files to test folder

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,8 +4,7 @@
 
 def roman_to_int(roman_string):
     """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
+    if (not isinstance(roman_string, str) or roman_string is None):
         return (0)
 
     values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
@@ -18,7 +17,6 @@ def roman_to_int(roman_string):
         if (i != (len(roman_string) - 1) and
                 values[roman_string[i]] < values[roman_string[i + 1]]):
             num += values[roman_string[i]] * -1
-
         else:
             num += values[roman_string[i]]
     return (num)
