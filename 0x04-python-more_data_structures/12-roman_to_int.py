cd0c49a985482004ff4bf8083b25282e3bee8df7
GoodnessJames <akomagoodness97@gmail.com>
2023-07-21 16:34:49 +0100

    Fix SyntaxError

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@
 
 def roman_to_int(roman_string):
     """Converts a roman numeral to an integer."""
-    if (type roman_string != str or roman_string is None):
+    if type(roman_string != str) or roman_string is None:
         return (0)
 
     values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
