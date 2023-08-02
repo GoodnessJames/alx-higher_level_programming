77855e9fa2e9cee9e8417c00b2957df5930a695f
GoodnessJames <akomagoodness97@gmail.com>
2023-08-02 02:24:22 +0100

    Fix missing continuation indentation error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,8 +39,8 @@ class Square:
     def position(self, value):
         """Set the current size of the square."""
         if type(value) is not tuple or len(value) != 2 or\
-        type(value[0]) is not int or type(value[1]) is not int or\
-        value[0] < 0 or value[1] < 0:
+                type(value[0]) is not int or type(value[1]) is not int or\
+                value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
