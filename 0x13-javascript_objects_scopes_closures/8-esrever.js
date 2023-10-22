18abd5ec7555f91045dfc8745eda03c53566f627
GoodnessJames <akomagoodness97@gmail.com>
2023-10-22 17:20:27 +0100

    JS objects, scopes and closures

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let beg = 0;
+  let start = 0;
   let end = list.length - 1;
-  while (beg < end) {
-    const tmp = list[beg];
-    list[beg] = list[end];
+  while (start < end) {
+    const tmp = list[start];
+    list[start] = list[end];
     list[end] = tmp;
-    beg++;
+    start++;
     end--;
   }
   return list;
