ecb0d4070214b6c20d357546553833b67ce113d8
GoodnessJames <akomagoodness97@gmail.com>
2023-11-29 05:32:29 +0100

    Fix bug

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,25 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  for (const character of characters) {
+    request(character, (err, response, body) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const display = JSON.parse(body);
+      console.log(display.name);
     });
   }
 });
